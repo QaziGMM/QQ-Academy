31f36a08f8f61e9fba2a07394fe1c416c59a3c9e
sohail <qazisohailabbas5512@gmail.com>
2025-06-19 15:51:10 +0500

    changes

--- src/components/TransitionWraper.jsx
+++ src/components/TransitionWraper.jsx
@@ -1,24 +1,29 @@
-// src/components/SlideRightWrapper.jsx
+// src/components/SlideSpringWrapper.jsx
 import { motion } from "framer-motion";
 
-const slideLeftVariants = {
-  initial: { x: "+100%", opacity: 0 },
-  animate: { x: 0, opacity: 1 },
-  exit: { x: "100%", opacity: 0 },
+const pageVariants = {
+  initial: { opacity: 0, x: 100 },
+  animate: { opacity: 1, x: 0 },
+  exit: { opacity: 0, x: -100 },
 };
+
 
 const TransitionWraper = ({ children }) => {
   return (
     <motion.div
-      variants={slideLeftVariants}
+      variants={pageVariants}
       initial="initial"
       animate="animate"
       exit="exit"
-      transition={{ duration: 0.9, ease: "easeInOut" }}
+      transition={{
+        type: "spring",
+        stiffness: 60,   // speed of bounce (lower = smoother)
+        damping: 14,     // how much bounce settles (higher = less bouncy)
+      }}
     >
       {children}
     </motion.div>
   );
 };
 
-export default TransitionWraper ;
+export default TransitionWraper;
